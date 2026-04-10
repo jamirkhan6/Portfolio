e1551dd3980101657bca151059c5f468b63ef96f
jamirkhan6 <ajzomirkhan6@gmail.com>
2026-04-10 10:24:57 +0600

    git link add

--- backend/src/tools/getGithubRepo.tool.js
+++ backend/src/tools/getGithubRepo.tool.js
@@ -1,9 +1,7 @@
 const axios = require('axios')
 
 async function getGithubRepo() {
-  const res = await axios.get(
-    "https://api.github.com/users/yourusername/repos"
-  );
+  const res = await axios.get("https://github.com/jamirkhan6");
   return res.data.map((p) => ({
     name: p.name,
     language: p.language,
